3d4104bb92a5acf136ba9f90af6575c6c8b2c6af
muffindud <ccatlabuga2002@gmail.com>
2024-11-10 14:30:40 +0200

    add correlation matrix gen

--- lab4/main.py
+++ lab4/main.py
@@ -1,16 +1,27 @@
 from pandas import read_csv
 from json import dumps
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 from src.utils import *
 
 
-# DATA_PATH = "data/data.csv"
 DATA_PATH = "data/cleaned_data.csv"
-data = read_csv(DATA_PATH)
+try:
+    data = read_csv(DATA_PATH)
+except FileNotFoundError:
+    DATA_PATH = "data/data.csv"
+    data = read_csv(DATA_PATH)
+    data = clean_data(data)
+    data.to_csv("data/cleaned_data.csv", index=False)
 
 
 def main():
-    ...
+    correlation_matrix = get_correlation_matrix(data)
+
+    plt.title("Correlation Matrix")
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0, vmin=-1, vmax=1, square=True)
+    plt.savefig("plots/correlation_matrix.png", bbox_inches="tight", dpi=300)
 
 
 if __name__ == "__main__":
--- lab4/src/utils.py
+++ lab4/src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def get_unique_years(data: pd.DataFrame) -> list:
@@ -6,14 +7,28 @@ def get_unique_years(data: pd.DataFrame) -> list:
 
 
 def get_unique_job_titles(data: pd.DataFrame) -> list:
-    return list(set(data["JobTitle"]))
-
-
-def get_unique_job_titles(data: pd.DataFrame) -> list:
     jobs: list[str] = list(data["JobTitle"])
     for i in range(len(jobs)):
         jobs[i] = jobs[i].upper()
     return list(set(jobs))
+
+
+def get_correlation_matrix(data: pd.DataFrame) -> pd.DataFrame:
+    """Get the level of correlation between the columns of the data.
+
+    :param data: The data to analyze.
+    :return: A list of lists, where each list represents the correlation between the columns of the data.
+    """
+
+    data = data.apply(pd.to_numeric, errors="coerce")
+
+    correlation_matrix = data.corr()
+    correlation_matrix = correlation_matrix.dropna(axis=0, how="all")
+    correlation_matrix = correlation_matrix.dropna(axis=1, how="all")
+    correlation_matrix = correlation_matrix.round(2)
+    correlation_matrix = correlation_matrix.iloc[1:, 1:]
+
+    return correlation_matrix
 
 
 def clean_data(data: pd.DataFrame) -> pd.DataFrame:
